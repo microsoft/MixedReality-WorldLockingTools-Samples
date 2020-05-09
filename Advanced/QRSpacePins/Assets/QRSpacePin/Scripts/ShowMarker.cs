3538b516eeb3070d4e952d48ed77e050b599dd98
Mark Finch <mafinc@microsoft.com>
2020-05-09 14:05:20 +0100

    Initial version of QRSpacePins sample.

---
+++ Advanced/QRSpacePins/Assets/QRSpacePin/Scripts/ShowMarker.cs
@@ -0,0 +1,153 @@
+﻿//#define BARN_DISABLE_AT_END
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShowMarker : MonoBehaviour
+{
+    public float rampUp = 1.0f;
+
+    public float rampDown = 5.0f;
+
+    public float maxSize = 4.0f;
+
+    private float age = 0.0f;
+
+    private struct AnimMat
+    {
+        public Material material;
+
+        public Color diffuse;
+
+        public Color emissive;
+
+    }
+    private readonly SortedSet<AnimMat> materials = new SortedSet<AnimMat>();
+
+    private Vector3 initScale;
+
+    private readonly string diffuseName = "_Color";
+    private readonly string emissiveName = "_EmissiveColor";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var renderers = GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < renderers.Length; ++i)
+        {
+            var rend = renderers[i];
+            AnimMat animMat = new AnimMat()
+            {
+                material = rend.material,
+                diffuse = rend.material.GetColor(diffuseName),
+                emissive = rend.material.GetColor(emissiveName),
+            };
+            rend.material.SetColor(diffuseName, Color.black);
+            SetIntensity(0.0f, animMat);
+            initScale = transform.localScale;
+            materials.Add(animMat);
+        }
+        age = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (age < rampUp)
+        {
+            RampUp(age);
+        }
+        else if (age < rampUp + rampDown)
+        {
+            RampDown(age);
+        }
+        else
+        {
+            SetStatic();
+        }
+        age += Time.deltaTime;
+    }
+
+    private void SetStatic()
+    {
+#if BARN_DISABLE_AT_END
+        gameObject.SetActive(false);
+#else
+        for (var iter = materials.GetEnumerator(); iter.MoveNext();)
+        {
+            AnimMat animMat = iter.Current;
+            Color emissive = animMat.diffuse;
+            animMat.material.SetColor(emissiveName, emissive);
+        }
+#endif
+    }
+    private float SmoothStep(float zero, float one, float t)
+    {
+        t = t - zero;
+        t = t / (one - zero);
+        if (t <= 0)
+            return 0;
+        if (t >= 1)
+            return 1;
+        return (3 - 2 * t) * t * t;
+    }
+    private void RampUp(float age)
+    {
+        float intensity = SmoothStep(0.0f, 1.0f, age / rampUp); 
+        for (var iter = materials.GetEnumerator(); iter.MoveNext(); )
+        {
+            AnimMat animMat = iter.Current;
+            SetIntensity(intensity, animMat);
+        }
+        SetSizeFromAge(age);
+    }
+
+    private void RampDown(float age)
+    {
+        float intensity = SmoothStep(0.0f, 1.0f, 1.0f - (age - rampUp) / rampDown);
+        for (var iter = materials.GetEnumerator(); iter.MoveNext();)
+        {
+            AnimMat animMat = iter.Current;
+            
+            BlendIntensity(intensity, animMat);
+        }
+        SetSizeFromAge(age);
+    }
+
+    private void SetSizeFromAge(float age)
+    {
+        float t = age / (rampUp + rampDown);
+        t *= 2.0f;
+        t -= 1.0f;
+        t = Mathf.Abs(t);
+        t = 1.0f - t;
+        t = 1.0f + Mathf.Pow(t - 1.0f, 3.0f);
+        float minSize = 1.0f;
+        float size = minSize + t * (maxSize - minSize);
+
+        SetSize(size);
+    }
+
+    private void SetSize(float scale)
+    {
+        transform.localScale = initScale * scale;
+    }
+
+    private void SetIntensity(float intensity, AnimMat animMat)
+    {
+        Color emissive = animMat.emissive * intensity;
+        animMat.material.SetColor(emissiveName, emissive);
+    }
+
+    private void BlendIntensity(float intensity, AnimMat animMat)
+    {
+        Color emissive = animMat.emissive * intensity + animMat.diffuse * (1.0f - intensity);
+        animMat.material.SetColor(emissiveName, emissive);
+    }
+
+}
---
+++ Advanced/QRSpacePins/Assets/QRSpacePin/Scripts/SpacePinQRGroup.cs
@@ -0,0 +1,467 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Microsoft.MixedReality.WorldLocking.Core;
+using Microsoft.MixedReality.QR;
+
+namespace Microsoft.MixedReality.WorldLocking.Samples
+{
+    /// <summary>
+    /// A group of space pins with locations fed by reading QR codes placed in the physical environment.
+    /// </summary>
+    [RequireComponent(typeof(QRCodeMiniManager))]
+    public class SpacePinQRGroup : MonoBehaviour
+    {
+        /// <summary>
+        /// An orienter to infer orientation from position of pins. Shared over all pins.
+        /// </summary>
+        private IOrienter orienter;
+
+        [SerializeField]
+        [Tooltip("An orienter instance in the scene. If unset, one will be created.")]
+        private Orienter sharedOrienter = null;
+
+        /// <summary>
+        /// An orienter instance in the scene. If unset, one will be created.
+        /// </summary>
+        public Orienter SharedOrienter { get { return sharedOrienter; } set { sharedOrienter = value; orienter = value; } }
+
+        /// <summary>
+        /// Whether the enumeration completed event has been encountered yet.
+        /// </summary>
+        /// <remarks>
+        /// Added, Updated, and Removed events before enumerationFinished is true are cached,
+        /// not seen in the current session. These are ignored in favor of the SpacePin persistence
+        /// system instead.
+        /// </remarks>
+        private bool enumerationFinished = false;
+
+        private static readonly int trace = 0;
+        private static readonly int log = 5;
+        private static readonly int error = 10;
+
+        /// <summary>
+        /// A collection of everything needed to set a space pin from a detected QR code.
+        /// </summary>
+        private class SpacePinPackage
+        {
+            /// <summary>
+            /// The space pin to generate and maintain.
+            /// </summary>
+            public SpacePinOrientable spacePin = null;
+
+            /// <summary>
+            /// Helper class to transform QR code's pose into Unity's global space.
+            /// </summary>
+            public QRSpatialCoord coordinateSystem = null;
+
+            /// <summary>
+            /// The locked pose last sent to WLT space pin. 
+            /// </summary>
+            /// <remarks>
+            /// This is only used to detect when the read QR position has changed enough to resubmit.
+            /// </remarks>
+            private Pose lastLockedPose = Pose.identity;
+
+            /// <summary>
+            /// Whether the QR code pose has been submitted to the space pin yet.
+            /// </summary>
+            private bool isSet = false;
+
+            /// <summary>
+            /// Create a new space pin package.
+            /// </summary>
+            /// <param name="owner">The owning space pin group.</param>
+            /// <param name="virtualObject">Corresponding virtual object (for pose) in the scene.</param>
+            /// <returns>The created package.</returns>
+            /// <remarks>
+            /// The created space pin package is ready to deploy, but currently idle.
+            /// </remarks>
+            public static SpacePinPackage Create(SpacePinQRGroup owner, Transform virtualObject)
+            {
+                SimpleConsole.AddLine(log, $"CreatePinPackage on {virtualObject.name}");
+                SpacePinPackage package = new SpacePinPackage();
+                package.spacePin = virtualObject.gameObject.AddComponent<SpacePinOrientable>();
+                package.spacePin.Orienter = owner.orienter;
+
+                package.coordinateSystem = new QRSpatialCoord();
+
+                return package;
+            }
+
+            /// <summary>
+            /// Release all resources. Package is unusable after Release.
+            /// </summary>
+            public void Release()
+            {
+                SimpleConsole.AddLine(log, $"Release SpacePin {spacePin.name}");
+                Destroy(spacePin);
+                spacePin = null;
+            }
+
+            /// <summary>
+            /// Reset package to initial state. If space pin has been committed, it will be rescinded.
+            /// </summary>
+            public void Reset()
+            {
+                SimpleConsole.AddLine(log, $"Reset SpacePin {spacePin.name}");
+                spacePin.Reset();
+            }
+
+            /// <summary>
+            /// Attempt to set a space pin from the QR code.
+            /// </summary>
+            /// <param name="qrCode">The source QR code.</param>
+            /// <returns>True if a space pin was set from the current data.</returns>
+            /// <remarks>
+            /// Returning false does not necessarily mean an error occurred. For example, if the space pin
+            /// has already been set from the given QR code, and the location hasn't changed, no action
+            /// will be taken and the return value will be false. Or if the coordinate system is unable
+            /// to resolve the transform to global space, again the return will be false, indicating
+            /// trying again later. 
+            /// </remarks>
+            public bool Update(QRCode qrCode)
+            {
+                SimpleConsole.AddLine(trace, $"Update SpacePin {(coordinateSystem == null ? "null" : coordinateSystem.SpatialNodeId.ToString())}");
+                coordinateSystem.SpatialNodeId = qrCode.SpatialGraphNodeId;
+                Pose spongyPose;
+                if (!coordinateSystem.ComputePose(out spongyPose))
+                {
+                    return false;
+                }
+                Pose frozenPose = WorldLockingManager.GetInstance().FrozenFromSpongy.Multiply(spongyPose);
+                return UpdatePose(frozenPose);
+            }
+
+            /// <summary>
+            /// Given a new frozen pose, send it to SpacePin system if appropriate.
+            /// </summary>
+            /// <param name="frozenPose">New frozen space pose.</param>
+            /// <returns>True if the pose was pushed to the SpacePin system.</returns>
+            private bool UpdatePose(Pose frozenPose)
+            {
+                SimpleConsole.AddLine(trace, "UpdatePose");
+                bool didCommit = false;
+                var wltMgr = WorldLockingManager.GetInstance();
+                Pose lockedPose = wltMgr.LockedFromFrozen.Multiply(frozenPose);
+                if (NeedCommit(lockedPose))
+                {
+                    didCommit = CommitPose(frozenPose, lockedPose);
+                }
+                return didCommit;
+            }
+
+            /// <summary>
+            /// Commit the pose to the SpacePin system, deploying the highlight marker if one is specified.
+            /// </summary>
+            /// <param name="frozenPose">New pose in frozen space.</param>
+            /// <param name="lockedPose">New pose in locked space.</param>
+            /// <returns>True if pose successfully committed to SpacePin system.</returns>
+            private bool CommitPose(Pose frozenPose, Pose lockedPose)
+            {
+                SimpleConsole.AddLine(trace, $"Commit to {spacePin.name} F:{frozenPose} L:{lockedPose} S:{spacePin.transform.GetGlobalPose()}");
+                spacePin.SetFrozenPose(frozenPose);
+
+                isSet = true;
+                lastLockedPose = lockedPose;
+
+                SimpleConsole.AddLine(trace, "Deployed");
+                return true;
+            }
+
+            /// <summary>
+            /// Determine whether the pose should be committed to the space pin system.
+            /// </summary>
+            /// <param name="lockedPose">The pose to test, in locked space.</param>
+            /// <returns>True if a commit is indicated.</returns>
+            private bool NeedCommit(Pose lockedPose)
+            {
+                if (!isSet)
+                {
+                    SimpleConsole.AddLine(log, "Need commit because unset.");
+                    return true;
+                }
+                float RefreshThreshold = 0.01f; // one cm?
+                float distance = Vector3.Distance(lockedPose.position, lastLockedPose.position);
+                if ( distance > RefreshThreshold)
+                {
+                    SimpleConsole.AddLine(log, $"Need commit because new distance {distance}.");
+                    return true;
+                }
+                SimpleConsole.AddLine(trace, $"No commit.");
+                return false;
+            }
+        };
+
+        /// <summary>
+        /// The mini manager will issue qr event notifications from the main thread.
+        /// </summary>
+        private QRCodeMiniManager miniManager;
+
+        /// <summary>
+        /// One pin created for each spacePinPoint.
+        /// </summary>
+        private readonly List<SpacePinPackage> spacePins = new List<SpacePinPackage>();
+
+        [SerializeField]
+        [Tooltip("The virtual poses in the scene to be matched with the poses of the QR codes in the physical world.")]
+        private List<Transform> virtualMarkers = new List<Transform>();
+
+        /// <summary>
+        /// The virtual poses in the scene to be matched with the poses of the QR codes in the physical world.
+        /// </summary>
+        public List<Transform> VirtualMarkers { get { return virtualMarkers; } set { virtualMarkers = value; } }
+
+        /// <summary>
+        /// Clear back to the state before any QR codes had been detected.
+        /// </summary>
+        public void Reset()
+        {
+            for(int i = 0; i < spacePins.Count; ++i)
+            {
+                spacePins[i].Reset();
+            }
+        }
+
+        /// <summary>
+        /// Ensure all required components exist and cache references where appropriate.
+        /// </summary>
+        private void CheckComponents()
+        {
+            if (miniManager == null)
+            {
+                miniManager = GetComponent<QRCodeMiniManager>();
+            }
+            if (orienter == null)
+            {
+                if (sharedOrienter == null)
+                {
+                    orienter = gameObject.AddComponent<Orienter>();
+                }
+                else
+                {
+                    orienter = sharedOrienter;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prepare to activate.
+        /// </summary>
+        private void Start()
+        {
+            CheckComponents();
+        }
+
+        /// <summary>
+        /// Become active.
+        /// </summary>
+        private void OnEnable()
+        {
+            CheckComponents();
+
+            SetUpCallbacks();
+            SetUpSpacePins();
+            SimpleConsole.AddLine(trace, "QRSpacePin Enabled");
+        }
+
+        /// <summary>
+        /// Create the space pins in an idle state.
+        /// </summary>
+        /// <remarks>
+        /// Pins won't become active until the corresponding QR code is seen.
+        /// </remarks>
+        private void SetUpSpacePins()
+        {
+            DestroySpacePins();
+            for (int i = 0; i < VirtualMarkers.Count; ++i)
+            {
+                spacePins.Add(SpacePinPackage.Create(this, VirtualMarkers[i]));
+            }
+        }
+
+        /// <summary>
+        /// Free all resources created to support the space pins.
+        /// </summary>
+        /// <remarks>
+        /// After calling this, the group is no longer usable.
+        /// </remarks>
+        private void DestroySpacePins()
+        {
+            if (spacePins.Count > 0)
+            {
+                Debug.Assert(spacePins.Count == VirtualMarkers.Count, "Bad state, there should be no space pins, or one for each marker");
+                for (int i = 0; i < spacePins.Count; ++i)
+                {
+                    spacePins[i].Release();
+                }
+                spacePins.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Go into dormant state. Can be revived later.
+        /// </summary>
+        private void OnDisable()
+        {
+            TearDownCallbacks();
+        }
+
+        /// <summary>
+        /// Release resources in preparation for destruction. Cannot be revived.
+        /// </summary>
+        private void OnDestroy()
+        {
+            DestroySpacePins();
+        }
+
+        /// <summary>
+        /// Register for callbacks on QR code events. These callbacks will happen on the main thread.
+        /// </summary>
+        private void SetUpCallbacks()
+        {
+            Debug.Assert(miniManager != null, "Expected required component QRCodeMiniManager");
+
+            miniManager.OnQRAdded += OnQRCodeAdded;
+            miniManager.OnQRUpdated += OnQRCodeUpdated;
+            miniManager.OnQRRemoved += OnQRCodeRemoved;
+            miniManager.OnQREnumerated += OnQRCodeEnumerated;
+            SimpleConsole.AddLine(trace, "Callbacks SetUp");
+        }
+
+        /// <summary>
+        /// Unregister from callbacks.
+        /// </summary>
+        private void TearDownCallbacks()
+        {
+            miniManager.OnQRAdded -= OnQRCodeAdded;
+            miniManager.OnQRUpdated -= OnQRCodeUpdated;
+            miniManager.OnQRRemoved -= OnQRCodeRemoved;
+            miniManager.OnQREnumerated -= OnQRCodeEnumerated;
+            miniManager = null;
+            SimpleConsole.AddLine(trace, "Callbacks torn down");
+        }
+
+        /// <summary>
+        /// Check the validity of an index into the space pin packages.
+        /// </summary>
+        /// <param name="idx">The index to check.</param>
+        /// <returns>True if the index is valid.</returns>
+        private bool QRCodeIndexValid(int idx)
+        {
+            return idx >= 0 && idx < spacePins.Count;
+        }
+
+        /// <summary>
+        /// Process a newly added QR code.
+        /// </summary>
+        /// <param name="qrCode">The qr code to process.</param>
+        private void OnQRCodeAdded(QRCode qrCode)
+        {
+            SimpleConsole.AddLine(trace, $"OnAdded {qrCode.Data}, enumerated {enumerationFinished}");
+            if (enumerationFinished)
+            {
+                int idx = ExtractIndex(qrCode);
+                if (!QRCodeIndexValid(idx))
+                {
+                    return;
+                }
+                spacePins[idx].Update(qrCode);
+            }
+        }
+
+        /// <summary>
+        /// Process a newly updated QR code.
+        /// </summary>
+        /// <param name="qrCode">The qr code to process.</param>
+        private void OnQRCodeUpdated(QRCode qrCode)
+        {
+            SimpleConsole.AddLine(trace, $"OnAdded {qrCode.Data}, enumerated {enumerationFinished}");
+            if (enumerationFinished)
+            {
+                int idx = ExtractIndex(qrCode);
+                if (!QRCodeIndexValid(idx))
+                {
+                    return;
+                }
+                spacePins[idx].Update(qrCode);
+            }
+        }
+
+        /// <summary>
+        /// Process a newly removed QR code.
+        /// </summary>
+        /// <param name="qrCode">The qr code to process.</param>
+        private void OnQRCodeRemoved(QRCode qrCode)
+        {
+            SimpleConsole.AddLine(trace, $"OnQRCodeRemoved {qrCode.Data}");
+            int idx = ExtractIndex(qrCode);
+            if (!QRCodeIndexValid(idx))
+            {
+                return;
+            }
+            spacePins[idx].Reset();
+        }
+
+        /// <summary>
+        /// Process the enumeration completed event.
+        /// </summary>
+        /// <param name="qrCode"></param>
+        private void OnQRCodeEnumerated(QRCode qrCode)
+        {
+            Debug.Assert(qrCode == null, "Dummy qrCode parameter should always be null");
+            SimpleConsole.AddLine(trace, $"OnQRCodeEnumerated");
+            enumerationFinished = true;
+        }
+
+        /// <summary>
+        /// Extract a command and/or space pin index out of the qr code.
+        /// </summary>
+        /// <param name="qrCode"></param>
+        /// <returns>Space pin index corresponding to this qr code, or -1 if there isn't one.</returns>
+        /// <remarks>
+        /// Any commands found are processed immediately.
+        /// </remarks>
+        private int ExtractIndex(QRCode qrCode)
+        {
+            string code = qrCode.Data;
+
+            string[] tokens = code.Split(new string[] { "_" }, System.StringSplitOptions.None);
+
+            int qrIndex = GetQRCodeIndex(tokens);
+            if (!QRCodeIndexValid(qrIndex))
+            {
+                Debug.LogError($"Failed to parse id out of code {code}");
+                return -1;
+            }
+
+            return qrIndex;
+        }
+
+        /// <summary>
+        /// Extract the index from the qrCode data.
+        /// </summary>
+        /// <param name="codeSplitContents">The string to parse the index from.</param>
+        /// <returns>The index of the corresponding space pin.</returns>
+        /// <remarks>
+        /// Assumes the index is embedded in the qr code's data as the last element
+        /// in an underscore ('_') delimited string, e.g. "My_QRCode_03" would have
+        /// index 3, and "My_QR_Code_03_Wrong" would generate an error.
+        /// </remarks>
+        private int GetQRCodeIndex(string[] codeSplitContents)
+        {
+            int qrIndex = -1;
+            if (!int.TryParse(codeSplitContents[codeSplitContents.Length - 1], out qrIndex))
+            {
+                return -1;
+            }
+            qrIndex--;
+            return qrIndex;
+        }
+
+    }
+}
