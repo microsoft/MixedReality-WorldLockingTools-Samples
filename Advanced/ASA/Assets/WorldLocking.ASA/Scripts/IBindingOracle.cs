a9e52351c3ae48842035a14cd0bcb4e9f0f73844
Mark Finch <mafinc@microsoft.com>
2021-05-27 16:57:00 +0100

    Even more code documentation.

--- Advanced/ASA/Assets/WorldLocking.ASA/Scripts/IBindingOracle.cs
+++ Advanced/ASA/Assets/WorldLocking.ASA/Scripts/IBindingOracle.cs
@@ -8,13 +8,32 @@ using UnityEngine;
 
 namespace Microsoft.MixedReality.WorldLocking.ASA
 {
-
+    /// <summary>
+    /// Interface for a magical oracle that communicates IBinder bindings across space and time.
+    /// </summary>
+    /// <remarks>
+    /// Note that the IBindingOracle only transmits bindings, which are string pairs of SpacePinId and CloudAnchorId.
+    /// It does not cause the binder to do anything with those bindings.
+    /// </remarks>
     public interface IBindingOracle
     {
+        /// <summary>
+        /// The name of this oracle.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Broadcast the bindings.
+        /// </summary>
+        /// <param name="binder">The binder whose bindings should be broadcast.</param>
+        /// <returns>True on success.</returns>
         bool Put(IBinder binder);
 
+        /// <summary>
+        /// Retrieve bindings from the ethereal plane.
+        /// </summary>
+        /// <param name="binder">The binder to add the bindings to.</param>
+        /// <returns>True on success.</returns>
         bool Get(IBinder binder);
     }
 }
--- Advanced/ASA/Assets/WorldLocking.ASA/Scripts/SpacePinBinderFile.cs
+++ Advanced/ASA/Assets/WorldLocking.ASA/Scripts/SpacePinBinderFile.cs
@@ -8,24 +8,50 @@ using UnityEngine;
 
 namespace Microsoft.MixedReality.WorldLocking.ASA
 {
-
+    /// <summary>
+    /// Simple implementation of an IBindingOracle interface, which puts and gets binding from file locally.
+    /// </summary>
+    /// <remarks>
+    /// Note that this implements spatial persistence locally across sessions.
+    /// Also, the file can be transferred from device to device, allowing shared spaces
+    /// between HoloLens, Android, and iOS.
+    /// </remarks>
     public class SpacePinBinderFile : MonoBehaviour, IBindingOracle
     {
-
+        [Tooltip("The name of the file into which bindings will be written and from which they will be read.")]
         [SerializeField] 
         private string fileName = "BinderFile.txt";
 
+        /// <summary>
+        /// Name of this oracle.
+        /// </summary>
         public string Name { get { return name; } }
 
+        /// <summary>
+        /// Retrieve bindings from file and apply them to the input binder.
+        /// </summary>
+        /// <param name="binder">Binder to apply them to.</param>
+        /// <returns>True on success.</returns>
         public bool Get(IBinder binder)
         {
             return Load(binder);
         }
 
+        /// <summary>
+        /// Pull bindings from the binder and save them to file.
+        /// </summary>
+        /// <param name="binder">Binder to pull from.</param>
+        /// <returns>True on success.</returns>
         public bool Put(IBinder binder)
         {
             return Save(binder);
         }
+
+        /// <summary>
+        /// Implement Put().
+        /// </summary>
+        /// <param name="binder">Binder whose bindings are to be saved.</param>
+        /// <returns>True on success.</returns>
         private bool Save(IBinder binder)
         {
             if (binder == null)
@@ -45,6 +71,11 @@ namespace Microsoft.MixedReality.WorldLocking.ASA
             return true;
         }
 
+        /// <summary>
+        /// Implement Get().
+        /// </summary>
+        /// <param name="binder">Binder to apply bindings to.</param>
+        /// <returns>True on success.</returns>
         private bool Load(IBinder binder)
         {
             if (binder == null)
@@ -73,6 +104,10 @@ namespace Microsoft.MixedReality.WorldLocking.ASA
             return true;
         }
 
+        /// <summary>
+        /// Convert fileName to a full path name.
+        /// </summary>
+        /// <returns>The full path name.</returns>
         private string GetFullPath()
         {
             string path = Application.persistentDataPath;
